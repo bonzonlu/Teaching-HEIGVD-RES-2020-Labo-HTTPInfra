59d2c2548279e408bf5c37770ec523aa3ff94d86
Doran Kayoumi <dorankayoumi@gmail.com>
2020-05-10 17:38:06 +0200

    Fetch new hashtag every 3 seconds

--- docker-images/apache-php-image/content/js/app.js
+++ docker-images/apache-php-image/content/js/app.js
@@ -1,13 +1,23 @@
 const target = document.getElementById('hashtag');
 
-fetch("/api/hashtag/")
-  // convert the response to json
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    target.innerHTML = data;
-  })
-  .catch(() => {
-    console.log("API unreachable");
-  });
+// fetch the first hashtag
+fetchHashtag();
+// update it every 3 seconds
+setInterval(fetchHashtag, 3000);
+
+/**
+ * Fetch a hashtag from the api
+ */
+function fetchHashtag() {
+  fetch("/api/hashtag/")
+    // convert the response to json
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      target.innerHTML = data;
+    })
+    .catch(() => {
+      console.log("API unreachable");
+    });
+}
